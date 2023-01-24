c1044fd0669829d4b68fd2dc4601b7404ee1b2d1
ltlaitoff <ltlaitoff@gmail.com>
2023-01-25 01:54:01 +0200

    feat: Connected mongo db to project

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
 import express, { Express, Request, Response } from 'express'
 import dotenv from 'dotenv'
+import { connect } from 'mongoose'
 
 dotenv.config()
+
+connect(
+	`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@counterclaster.9imvrz0.mongodb.net/${process.env.MONGO_DATABASE}?retryWrites=true&w=majority`
+)
+	.then(() => {
+		console.log('[Server]: DB connected')
+	})
+
+	.catch(err => {
+		console.error('[Server]: DB error connect', err)
+	})
 
 const app: Express = express()
 const port = process.env.PORT
