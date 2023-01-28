d5ca90e4ecaf251dd092b0d3af3e6b10fc7cdb3d
ltlaitoff <ltlaitoff@gmail.com>
2023-01-28 17:50:00 +0200

    feat: Created Authorization/authorization contoller

---
+++ src/controllers/Authorization/authorization.ts
@@ -0,0 +1,49 @@
+import { Response, Request } from 'express'
+import * as jose from 'jose'
+
+import { findOrCreateUser } from '../../controllers/User/findOrCreateUser'
+import { authorizationError, somethingWentWrongError } from '../../services'
+import { getUserDataFromJWTPayload } from './getUserDataFromJWTPayload.helper'
+
+export const authorization = (req: Request, res: Response) => {
+	const authorization = req.get('authorization')
+	if (!authorization) return authorizationError(res)
+
+	let jwkGoogleKeys = null
+
+	try {
+		jwkGoogleKeys = jose.createRemoteJWKSet(
+			new URL(process.env.GOOGLE_JWK_PUBLIC_KEYS)
+		)
+	} catch (error) {
+		return somethingWentWrongError(res)
+	}
+
+	jose
+		.jwtVerify(authorization, jwkGoogleKeys)
+		.then(value => {
+			const userData = getUserDataFromJWTPayload(value.payload)
+
+			if (userData === null) {
+				return somethingWentWrongError(res)
+			}
+
+			findOrCreateUser(userData)
+				.then(value => {
+					req.session.authorized = true
+					req.session.name = value.name
+					req.session.email = value.email
+
+					console.log(req.session, value._id)
+					res.status(200).json(value)
+				})
+				.catch((error: Error) => {
+					console.log('ERROR', error.name)
+					return somethingWentWrongError(res)
+				})
+		})
+		.catch((error: Error) => {
+			console.error('ERROR: ', error.name)
+			return somethingWentWrongError(res)
+		})
+}
