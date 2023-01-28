93ab24587979f9fc7034cd0ce2ef4bb73bbec570
ltlaitoff <ltlaitoff@gmail.com>
2023-01-28 14:35:20 +0200

    User model. Removed: username, key; Added: name, picture, email_verified, given_name, family_name

--- src/models/User.ts
+++ src/models/User.ts
@@ -2,9 +2,12 @@ import { Schema, model } from 'mongoose'
 
 const userSchema = new Schema(
 	{
-		username: { type: String, required: true },
+		name: { type: String, required: true },
+		picture: { type: String, required: true },
 		email: { type: String, required: true },
-		key: { type: String, required: true }
+		email_verified: { type: Boolean, required: true },
+		given_name: { type: String, required: true },
+		family_name: { type: String, required: true }
 	},
 	{
 		timestamps: true
