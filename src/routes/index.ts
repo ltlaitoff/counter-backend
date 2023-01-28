bf09d4b32b347c132f4c78874edc01d9e21d1650
ltlaitoff <ltlaitoff@gmail.com>
2023-01-28 19:43:15 +0200

    feat: Connected initialize router to rootRouter

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 import authorizationRouter from './authorization'
 import debugRouter from './debug'
+import initializeRouter from './initialize'
 
 const rootRouter = Router()
 
 rootRouter.use(authorizationRouter)
+rootRouter.use(initializeRouter)
 rootRouter.use('/', debugRouter)
 
 export default rootRouter
