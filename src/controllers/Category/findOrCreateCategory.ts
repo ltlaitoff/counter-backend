bc8234d1ca032d6833c73c549ecdec32f63c7f7d
ltlaitoff <ltlaitoff@gmail.com>
2023-01-29 12:37:33 +0200

    feat: Created Category/findOrCreateCategory

---
+++ src/controllers/Category/findOrCreateCategory.ts
@@ -0,0 +1,14 @@
+import { ICategory } from '../../models'
+import { findCategory, createCategory } from '.'
+
+export const findOrCreateCategory = async (data: ICategory) => {
+	const findedCategory = await findCategory(data)
+
+	if (findedCategory !== null) {
+		return findedCategory
+	}
+
+	const createdCategory = await createCategory(data)
+
+	return createdCategory
+}
