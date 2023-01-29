b6284a0d5a5a53e8425397103d20d7998a46c44a
ltlaitoff <ltlaitoff@gmail.com>
2023-01-29 12:32:26 +0200

    feat: Created config/consts DEFAULT_COLORS

---
+++ src/config/consts.ts
@@ -0,0 +1,16 @@
+import { ColorsNames } from '../types/Color.types'
+
+export const DEFAULT_COLORS = [
+	{
+		name: ColorsNames.RED,
+		colorHEX: '#ff0000'
+	},
+	{
+		name: ColorsNames.GREEN,
+		colorHEX: '#00ff00'
+	},
+	{
+		name: ColorsNames.BLUE,
+		colorHEX: '#0000ff'
+	}
+]
