df436db49511a71fd0f25917183dff13ca352922
ltlaitoff <ltlaitoff@gmail.com>
2023-01-29 12:32:51 +0200

    feat: Created Color/initiaizeDefaultColors

---
+++ src/controllers/Color/initializeDefaultColors.ts
@@ -0,0 +1,22 @@
+import { findOrCreateColor } from '.'
+import { ObjectId } from 'mongoose'
+import { ColorsNames } from '../../types/Color.types'
+import { DEFAULT_COLORS } from '../../config'
+
+export const initializeDefaultColors = async () => {
+	const colorItems = await Promise.all(
+		DEFAULT_COLORS.map(defaultColorItem => {
+			return findOrCreateColor(defaultColorItem)
+		})
+	)
+
+	const resultObject: any = {}
+
+	colorItems.forEach(item => {
+		if (item._id) {
+			resultObject[item.name] = item._id
+		}
+	})
+
+	return resultObject as { [key in ColorsNames]: ObjectId }
+}
