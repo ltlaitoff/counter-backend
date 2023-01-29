2b83b4be32a552b4c067ddcad8cef340b0f47f7d
ltlaitoff <ltlaitoff@gmail.com>
2023-01-29 12:37:33 +0200

    feat: Created Category/intializeUserDefaultCategories

---
+++ src/controllers/Category/intializeUserDefaultCategories.ts
@@ -0,0 +1,35 @@
+import { ObjectId } from 'mongoose'
+import { ColorsNames } from '../../types/Color.types'
+import { initializeDefaultColors } from '../Color'
+import { findOrCreateCategory } from '.'
+
+export const intializeUserDefaultCategories = async (userId: ObjectId) => {
+	const colors = await initializeDefaultColors()
+
+	const DEFAULT_CATEGORIES = [
+		{
+			name: 'Pull-ups',
+			comment: 'Pull ups count',
+			color: colors[ColorsNames.RED],
+			order: 1
+		},
+		{
+			name: 'Push-ups',
+			comment: 'Push ups count',
+			color: colors[ColorsNames.GREEN],
+			order: 2
+		},
+		{
+			name: 'Wall slides',
+			comment: 'Pull ups count',
+			color: colors[ColorsNames.BLUE],
+			order: 3
+		}
+	]
+
+	console.log(DEFAULT_CATEGORIES)
+
+	return DEFAULT_CATEGORIES.map(defaultCategory => {
+		return findOrCreateCategory({ user: userId, ...defaultCategory })
+	})
+}
