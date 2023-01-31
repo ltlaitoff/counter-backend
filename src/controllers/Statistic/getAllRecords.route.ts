76cd8c61cef87cd0d0cd83246491c719ca324f10
ltlaitoff <ltlaitoff@gmail.com>
2023-01-31 23:20:42 +0200

    feat: Created Statistic/getAllRecords.route

---
+++ src/controllers/Statistic/getAllRecords.route.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from 'express'
+
+import { getSessionUserId } from 'helpers'
+import { Statistic } from 'models/Statistic'
+import { unauthorizedError, somethingWentWrongError } from 'services'
+import HttpStatusCode from 'types/HttpStatusCode'
+
+export const getAllRecordsRoute = (req: Request, res: Response) => {
+	const userId = getSessionUserId(req.session)
+
+	if (userId === null) {
+		return unauthorizedError(res)
+	}
+
+	return Statistic.find({ _id: userId }).exec((error, value) => {
+		if (error) {
+			return somethingWentWrongError(res)
+		}
+
+		return res.status(HttpStatusCode.OK).json(value)
+	})
+}
