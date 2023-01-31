cf6c6bd4895d7fe172d5c15ff39a23cb464bd6cf
ltlaitoff <ltlaitoff@gmail.com>
2023-01-31 22:56:56 +0200

    feat: Created unauthorizedError service

---
+++ src/services/Errors/unauthorizedError.ts
@@ -0,0 +1,9 @@
+import { Response } from 'express'
+import HttpStatusCode from 'types/HttpStatusCode'
+
+/**
+ * Used on each request where the user unauthorized
+ */
+export const unauthorizedError = (res: Response) => {
+	return res.status(HttpStatusCode.UNAUTHORIZED).json({ error: 'Unauthorized' })
+}
