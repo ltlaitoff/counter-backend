11665e24f71baa86a5e211736a5aa21622ecf46a
ltlaitoff <ltlaitoff@gmail.com>
2023-01-31 23:25:28 +0200

    feat: Connected statistic router to rootRouter

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express'
 import authorizationRouter from './authorization'
 import debugRouter from './debug'
 import initializeRouter from './initialize'
+import statisticRouter from './statistic'
 
 const rootRouter = Router()
 
 rootRouter.use(authorizationRouter)
 rootRouter.use(initializeRouter)
+rootRouter.use(statisticRouter)
 rootRouter.use('/', debugRouter)
 
 export default rootRouter
