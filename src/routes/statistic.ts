53fdf61af79f5060b8af4123fe44da24b00890b8
ltlaitoff <ltlaitoff@gmail.com>
2023-01-31 23:36:16 +0200

    feat: Added getByRecordId route in statisticRouter

--- src/routes/statistic.ts
+++ src/routes/statistic.ts
@@ -4,5 +4,6 @@ import { routeContollers as statisticContoller } from 'controllers/Statistic'
 const statisticRouter = Router()
 
 statisticRouter.get('/statistic/all', statisticContoller.getAllRecords)
+statisticRouter.get('/statistic/:recordId', statisticContoller.getByRecordId)
 
 export default statisticRouter
