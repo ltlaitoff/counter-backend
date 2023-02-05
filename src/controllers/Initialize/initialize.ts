4deaaa655e8a46543c8a45644c29918504e09c44
ltlaitoff <ltlaitoff@gmail.com>
2023-02-05 18:23:33 +0200

    feat: Added authorized field in initialize

--- src/controllers/Initialize/initialize.ts
+++ src/controllers/Initialize/initialize.ts
@@ -2,10 +2,15 @@ import { Response, Request } from 'express'
 import HttpStatusCode from 'types/HttpStatusCode'
 import { getUserById } from 'controllers/User'
 
-export const initialize = (req: Request, res: Response) => {
+export const initialize = async (req: Request, res: Response) => {
 	if (!req.session.authorized || !req.session.userId) {
 		return res.status(HttpStatusCode.OK).json({ authorized: false })
 	}
 
-	return getUserById(req.session.userId)
+	const user = await getUserById(req.session.userId)
+
+	res.status(HttpStatusCode.OK).json({
+		authorized: true,
+		...user
+	})
 }
