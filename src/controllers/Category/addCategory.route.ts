7db262feb48a1f32a707e57b6ce74118e8cbfc90
ltlaitoff <ltlaitoff@gmail.com>
2023-02-07 19:14:09 +0200

    feat: Created Category/addCategory

---
+++ src/controllers/Category/addCategory.route.ts
@@ -0,0 +1,62 @@
+import { Request, Response } from 'express'
+
+import { getSessionUserId, stringValidation } from 'helpers'
+import { unauthorizedError } from 'services'
+
+import { Category, ICategory } from 'models/Category'
+import {} from '../../helpers/validators/string'
+import { getLastCategoryOrder } from './getLastCategoryOrder'
+
+type RecordBody = Omit<ICategory, 'color' | 'user' | 'order'> & {
+	color: string
+}
+
+const recordBodyValidation = (body: Request['body']): null | RecordBody => {
+	const name = body.name
+	const comment = body.comment
+	const color = body.color
+
+	const resultName = stringValidation(name)
+	if (resultName === null) return null
+
+	const resultComment = stringValidation(comment)
+	if (resultComment === null) return null
+
+	const resultColor = stringValidation(color)
+	if (resultColor === null) return null
+
+	return {
+		name: resultName,
+		comment: resultComment,
+		color: resultColor
+	}
+}
+
+export const addCategoryRoute = async (req: Request, res: Response) => {
+	console.log(req.body)
+	const validBodyData = recordBodyValidation(req.body)
+
+	if (validBodyData === null) {
+		return res.status(404).json({ message: 'Not valid body' })
+	}
+
+	const userId = getSessionUserId(req.session)
+
+	if (userId === null) {
+		return unauthorizedError(res)
+	}
+
+	const lastOrderId = await getLastCategoryOrder()
+
+	const dataForAdd: Omit<ICategory, 'color'> & {
+		color: string
+	} = {
+		user: userId,
+		...validBodyData,
+		order: lastOrderId === null ? 0 : lastOrderId + 1
+	}
+
+	Category.create(dataForAdd, (error, document) => {
+		res.status(200).json(document)
+	})
+}
