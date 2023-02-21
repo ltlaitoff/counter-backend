4006c8b84e54e43260f673ff10ce72a1619107c0
ltlaitoff <ltlaitoff@gmail.com>
2023-02-21 23:36:32 +0200

    feat: Added ...args in utils/debugConsole

--- src/utils/debugConsole.util.ts
+++ src/utils/debugConsole.util.ts
@@ -1,11 +1,18 @@
 import chalk from 'chalk'
 
-export const debugMessage = (message: string, tag?: string) => {
-	console.log(`${tag + ': ' || ''}${message}`)
+export const debugMessage = (
+	message: string,
+	tag?: string,
+	...args: Array<unknown>
+) => {
+	console.log(`${tag + ': ' || ''}${message}`, ...args)
 }
 
-export const serverDebugMessage = (message: string) => {
+export const serverDebugMessage = (
+	message: string,
+	...args: Array<unknown>
+) => {
 	const tag = chalk.yellowBright(`[Server]`)
 
-	debugMessage(message, tag)
+	debugMessage(message, tag, ...args)
 }
