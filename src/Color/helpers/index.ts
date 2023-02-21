54dfcde82fef8f2036a14607d19a0b26ded928e9
ltlaitoff <ltlaitoff@gmail.com>
2023-02-22 00:49:45 +0200

    feat(Color): Added Color/helpers reexport createOrUpdateColors, removeDuplicates and removeUnUsedColors

--- src/Color/helpers/index.ts
+++ src/Color/helpers/index.ts
@@ -1,2 +1,5 @@
 export { findOrCreateColor } from './findOrCreateColor.helper'
 export { initializeDefaultColors } from './initializeDefaultColors.helper'
+export { removeUnUsedColors } from './removeUnUsedColors.helper'
+export { createOrUpdateColors } from './createOrUpdateColors.helper'
+export { removeDuplicates } from './removeDuplicates.helper'
