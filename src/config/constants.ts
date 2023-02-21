dd0d1a8e92836877496d6d860248d2fbf6ff9629
ltlaitoff <ltlaitoff@gmail.com>
2023-02-22 00:05:35 +0200

    feat: Update Colors constants

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,16 +1,55 @@
 import { ColorTypes } from 'Color'
 
+/*
+	TODO: Create default colors
+*/
 export const DEFAULT_COLORS = [
 	{
 		name: ColorTypes.ColorsNames.RED,
-		colorHEX: '#ff0000'
-	},
-	{
-		name: ColorTypes.ColorsNames.GREEN,
-		colorHEX: '#00ff00'
+		colorHEX: '#f87171'
 	},
 	{
 		name: ColorTypes.ColorsNames.BLUE,
-		colorHEX: '#0000ff'
+		colorHEX: '#60a5fa'
+	},
+	{
+		name: ColorTypes.ColorsNames.SLATE,
+		colorHEX: '#475569'
+	},
+	{
+		name: ColorTypes.ColorsNames.ORANGE,
+		colorHEX: '#fb923c'
+	},
+	{
+		name: ColorTypes.ColorsNames.LIME,
+		colorHEX: '#a3e635'
+	},
+	{
+		name: ColorTypes.ColorsNames.GREEN,
+		colorHEX: '#4ade80'
+	},
+	{
+		name: ColorTypes.ColorsNames.EMERALD,
+		colorHEX: '#34d399'
+	},
+	{
+		name: ColorTypes.ColorsNames.ROSE,
+		colorHEX: '#fb7185'
+	},
+	{
+		name: ColorTypes.ColorsNames.PINK,
+		colorHEX: '#ec4899'
+	},
+	{
+		name: ColorTypes.ColorsNames.PURPLE,
+		colorHEX: '#c084fc'
+	},
+	{
+		name: ColorTypes.ColorsNames.INDIGO,
+		colorHEX: '#818cf8'
+	},
+	{
+		name: ColorTypes.ColorsNames.SKY,
+		colorHEX: '#38bdf8'
 	}
 ]
