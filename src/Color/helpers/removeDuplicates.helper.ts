d3088a1b0bcbafd4cad5231a20a42954ad131260
ltlaitoff <ltlaitoff@gmail.com>
2023-02-22 00:49:45 +0200

    feat(Color): Created Color/helpers/removeDuplicates.helper

---
+++ src/Color/helpers/removeDuplicates.helper.ts
@@ -0,0 +1,25 @@
+import { ObjectId } from 'mongoose'
+
+import { ColorTypes } from 'Color'
+import { ColorServices } from '..'
+
+type ColorWithId = ColorTypes.Color & { _id: ObjectId }
+
+export const removeDuplicates = (all: ColorWithId[]) => {
+	const cache: Record<string, string> = {}
+
+	const forDelete: Array<ColorWithId> = []
+
+	all.forEach(item => {
+		if (item.name in cache) {
+			forDelete.push(item)
+			return
+		}
+
+		cache[item.name] = item.colorHEX
+	})
+
+	forDelete.forEach(color => {
+		ColorServices.removeColor(color)
+	})
+}
