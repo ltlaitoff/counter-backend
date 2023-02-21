e08995469f3172b3811a74d3e636e1d57eeb852b
ltlaitoff <ltlaitoff@gmail.com>
2023-02-21 23:34:33 +0200

    feat: Created utils/debugConsole serverDebugMessage function

--- src/utils/debugConsole.util.ts
+++ src/utils/debugConsole.util.ts
@@ -3,3 +3,9 @@ import chalk from 'chalk'
 export const debugMessage = (message: string, tag?: string) => {
 	console.log(`${tag + ': ' || ''}${message}`)
 }
+
+export const serverDebugMessage = (message: string) => {
+	const tag = chalk.yellowBright(`[Server]`)
+
+	debugMessage(message, tag)
+}
