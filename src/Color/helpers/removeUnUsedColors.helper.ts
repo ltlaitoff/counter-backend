a8ff96962481c0481d6a8859a0bf1a393cd01c2c
ltlaitoff <ltlaitoff@gmail.com>
2023-02-22 00:49:45 +0200

    feat(Color): Created Color/helpers/removeUnUsedColors.helper

---
+++ src/Color/helpers/removeUnUsedColors.helper.ts
@@ -0,0 +1,23 @@
+import { ObjectId } from 'mongoose'
+
+import { ColorTypes } from 'Color'
+import { ColorServices } from '..'
+
+export const removeUnUsedColors = (
+	all: (ColorTypes.Color & { _id: ObjectId })[],
+	defaults: ColorTypes.Color[]
+) => {
+	all.forEach(color => {
+		const findedDefaultColor = defaults.find(defaultColor => {
+			return (
+				defaultColor.name === color.name ||
+				defaultColor.colorHEX === color.colorHEX
+			)
+		})
+
+		if (findedDefaultColor === undefined) {
+			ColorServices.removeColor(color)
+			return
+		}
+	})
+}
