efbf560b8e4927531a2391fda958443ecd5f3018
ltlaitoff <ltlaitoff@gmail.com>
2023-02-22 00:49:46 +0200

    feat(Color): Rewrited Color/helpers initializeDefaultColors

--- src/Color/helpers/initializeDefaultColors.helper.ts
+++ src/Color/helpers/initializeDefaultColors.helper.ts
@@ -1,14 +1,16 @@
 import { ObjectId } from 'mongoose'
 
 import { DEFAULT_COLORS } from 'config'
-import { ColorTypes, ColorHelpers } from '..'
+import { ColorTypes, ColorServices, ColorHelpers } from '..'
 
 export const initializeDefaultColors = async () => {
-	const colorItems = await Promise.all(
-		DEFAULT_COLORS.map(defaultColorItem => {
-			return ColorHelpers.findOrCreateColor(defaultColorItem)
-		})
-	)
+	const allColors = await ColorServices.findColors()
+
+	ColorHelpers.createOrUpdateColors(allColors, DEFAULT_COLORS)
+	ColorHelpers.removeUnUsedColors(allColors, DEFAULT_COLORS)
+	ColorHelpers.removeDuplicates(allColors)
+
+	const colorItems = await ColorServices.findColors()
 
 	// TODO: Remove any type
 	const resultObject: any = {}
