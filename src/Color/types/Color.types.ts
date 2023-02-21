063164225d7fd9d8ff41eae42bff8ce852c8bab3
ltlaitoff <ltlaitoff@gmail.com>
2023-02-22 00:04:06 +0200

    feat: Update ColorNames in Color/types

--- src/Color/types/Color.types.ts
+++ src/Color/types/Color.types.ts
@@ -1,9 +1,21 @@
 import { ObjectId } from 'mongoose'
 
+/*
+	TODO: Find better method save colorNames
+*/
 export const enum ColorsNames {
 	RED = 'red',
+	BLUE = 'blue',
+	SLATE = 'slate',
+	ORANGE = 'orange',
+	LIME = 'lime',
 	GREEN = 'green',
-	BLUE = 'blue'
+	EMERALD = 'emerald',
+	ROSE = 'rose',
+	PINK = 'pink',
+	PURPLE = 'purple',
+	INDIGO = 'indigo',
+	SKY = 'sky'
 }
 
 export interface Color {
