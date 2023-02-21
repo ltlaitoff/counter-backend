27fa9c97221c5f975a1212d056e7712bcecc4083
ltlaitoff <ltlaitoff@gmail.com>
2023-02-22 00:44:06 +0200

    feat: Using debug messages in index

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,13 @@ import cors from 'cors'
 import session from 'express-session'
 import bodyParser from 'body-parser'
 import router from './routes'
+import { ColorHelpers } from 'Color'
+import { serverDebugMessage } from 'utils/debugConsole.util'
 
+/*
+	TODO: Create middleware for print to console requests:
+		[TYPE] User /../..
+*/
 declare module 'express-session' {
 	interface SessionData {
 		authorized: boolean
@@ -23,11 +29,11 @@ mongoose
 		`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@counterclaster.9imvrz0.mongodb.net/${process.env.MONGO_DATABASE}?retryWrites=true&w=majority`
 	)
 	.then(() => {
-		console.log('[Server]: DB connected')
+		serverDebugMessage('DB connected')
 	})
 
 	.catch(err => {
-		console.error('[Server]: DB error connect', err)
+		serverDebugMessage('DB error connect', err)
 	})
 
 const store = new session.MemoryStore()
@@ -56,5 +62,7 @@ app.use(bodyParser.json())
 app.use(router)
 
 app.listen(port, () => {
-	console.log(`⚡️[server]: Server is running at http://localhost:${port}`)
+	serverDebugMessage(`Server is running at http://localhost:${port}`)
+
+	ColorHelpers.initializeDefaultColors()
 })
