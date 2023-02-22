0ccb35053409ed970e8a0abd76ce7eab3234690f
ltlaitoff <ltlaitoff@gmail.com>
2023-02-22 19:37:54 +0200

    Fix(accounts error): Fixed creating dublicate accounts

--- src/User/helpers/findOrCreateUser.helper.ts
+++ src/User/helpers/findOrCreateUser.helper.ts
@@ -3,7 +3,23 @@ import { UserTypes, UserServices } from '..'
 export const findOrCreateUser = async (
 	data: UserTypes.User
 ): Promise<UserTypes.User> => {
-	const findedUser = await UserServices.findUser(data)
+	/*
+	 * ! SECURITY: On user change email or name system create new user account !
+	 */
+
+	/*
+		Google and update picture link to avatar
+		findUser must be only by email and name
+	*/
+	const dataForFind = {
+		name: data.name,
+		email: data.email,
+		email_verified: data.email_verified,
+		given_name: data.given_name,
+		family_name: data.family_name
+	}
+
+	const findedUser = await UserServices.findUser(dataForFind)
 
 	if (findedUser !== null) {
 		return findedUser
