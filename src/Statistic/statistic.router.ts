77fee8137dfca70056c90406f5cec2a65093ed84
ltlaitoff <ltlaitoff@gmail.com>
2023-03-11 05:32:20 +0200

    feat: Added deleteRecord.route to statistic.router

--- src/Statistic/statistic.router.ts
+++ src/Statistic/statistic.router.ts
@@ -6,5 +6,6 @@ const statisticRouter = Router()
 statisticRouter.get('/all', StatisticRoutes.getAllRecords)
 statisticRouter.get('/:recordId', StatisticRoutes.getByRecordId)
 statisticRouter.post('/add', StatisticRoutes.addRecord)
+statisticRouter.delete('/:id', StatisticRoutes.deleteRecord)
 
 export default statisticRouter
