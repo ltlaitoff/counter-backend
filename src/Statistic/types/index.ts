4ebee0758079fd6e03e1acb6fd4972d27aeb507d
ltlaitoff <ltlaitoff@gmail.com>
2023-03-11 05:31:01 +0200

    feat: Created DeleteRecord service Statistic type

--- src/Statistic/types/index.ts
+++ src/Statistic/types/index.ts
@@ -1,3 +1,3 @@
 export { Statistic } from './Statistic.types'
 export { FindOptions } from './Options.types'
-export { CreateRecord, FindRecord } from './Services.types'
+export { CreateRecord, FindRecord, DeleteRecord } from './Services.types'
