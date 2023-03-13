36848d1ae56a61433444117223ef4853db5e1f32
ltlaitoff <ltlaitoff@gmail.com>
2023-03-13 15:30:33 +0200

    fix(old category): category/all request

--- old-express-app/src/Category/routes/getAllCategories.route.ts
+++ old-express-app/src/Category/routes/getAllCategories.route.ts
@@ -5,23 +5,17 @@ import { CategoryModel } from '..'
 import { unauthorizedError, somethingWentWrongError } from '../../helpers'
 import { HttpStatusCode } from '../../types'
 
-export const getAllCategoriesRoute = (req: Request, res: Response) => {
+export const getAllCategoriesRoute = async (req: Request, res: Response) => {
 	const userId = getSessionUserId(req.session)
 
 	if (userId === null) {
 		return unauthorizedError(res)
 	}
 
-	return (
-		CategoryModel.find({ user: userId }, '-__v -createdAt -updatedAt')
-			.populate('color', '-__v -createdAt -updatedAt')
-			/* @ts-expect-error OLD */
-			.exec((error, value) => {
-				if (error) {
-					return somethingWentWrongError(res)
-				}
+	const result = await CategoryModel.find(
+		{ user: userId },
+		'-__v -createdAt -updatedAt'
+	).populate('color', '-__v -createdAt -updatedAt')
 
-				return res.status(HttpStatusCode.OK).json(value)
-			})
-	)
+	return res.status(HttpStatusCode.OK).json(result)
 }
