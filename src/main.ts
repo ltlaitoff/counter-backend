7b3d3dee827081c1ba7998e9269523715377bf98
ltlaitoff <ltlaitoff@gmail.com>
2023-03-20 08:04:36 +0200

    feat: Initialize basic swagger docs on /api route

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ExpressAdapter } from '@nestjs/platform-express'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 
 import expressApp from '../old-express-app/src'
 import { getHttpsOptions } from './helpers/getHttpsOptions.helper'
@@ -21,6 +22,15 @@ async function bootstrap() {
 		methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 		credentials: true
 	})
+
+	const config = new DocumentBuilder()
+		.setTitle('Counter')
+		.setDescription('The Counter API description')
+		.setVersion('1.0.0')
+		.build()
+
+	const document = SwaggerModule.createDocument(app, config)
+	SwaggerModule.setup('api', app, document)
 
 	await app.listen(process.env.PORT)
 
